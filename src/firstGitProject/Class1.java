dfc7febde026d8809c1a2a2d3c210e6c744fbca2
T460s <T460s@Thi>
2020-10-27 12:10:59 -0700

    Second Commit

--- src/firstGitProject/Class1.java
+++ src/firstGitProject/Class1.java
@@ -4,7 +4,10 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("Wazzup!\nFirst git project");
-
+		System.out.println("New code added here");
+		for (int i=1;i<=5;i++) {
+			System.out.print(i + " ");
+		}
 	}
 
 }
